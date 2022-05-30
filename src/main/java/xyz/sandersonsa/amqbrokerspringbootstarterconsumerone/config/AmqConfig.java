a4dd30afed70b00c54d7d8a4300acee79d58cd02
Sanderson Sa <ssa@redhat.com>
2022-05-30 08:16:59 -0300

    preservar msg

--- src/main/java/xyz/sandersonsa/amqbrokerspringbootstarterconsumerone/config/AmqConfig.java
+++ src/main/java/xyz/sandersonsa/amqbrokerspringbootstarterconsumerone/config/AmqConfig.java
@@ -1,53 +1,53 @@
-package xyz.sandersonsa.amqbrokerspringbootstarterconsumerone.config;
+// package xyz.sandersonsa.amqbrokerspringbootstarterconsumerone.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.annotation.JmsListenerConfigurer;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerEndpointRegistrar;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.jms.annotation.EnableJms;
+// import org.springframework.jms.annotation.JmsListenerConfigurer;
+// import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+// import org.springframework.jms.config.JmsListenerContainerFactory;
+// import org.springframework.jms.config.JmsListenerEndpointRegistrar;
+// import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+// import org.springframework.jms.support.converter.MessageConverter;
+// import org.springframework.jms.support.converter.MessageType;
 
-import javax.jms.ConnectionFactory;
-import javax.jms.Session;
+// import javax.jms.ConnectionFactory;
+// import javax.jms.Session;
 
-@EnableJms
-@Configuration
-@SuppressWarnings("SpringJavaAutowiringInspection")
-public class AmqConfig implements JmsListenerConfigurer {
+// @EnableJms
+// @Configuration
+// @SuppressWarnings("SpringJavaAutowiringInspection")
+// public class AmqConfig implements JmsListenerConfigurer {
 
-    // public static final String ORDER_QUEUE = "order-queue";
+//     // public static final String ORDER_QUEUE = "order-queue";
 
-    @Autowired
-    private ConnectionFactory connectionFactory;
+//     @Autowired
+//     private ConnectionFactory connectionFactory;
 
-    @Override
-    public void configureJmsListeners(JmsListenerEndpointRegistrar registrar) {
-        registrar.setContainerFactory(containerFactory());
-    }
+//     @Override
+//     public void configureJmsListeners(JmsListenerEndpointRegistrar registrar) {
+//         registrar.setContainerFactory(containerFactory());
+//     }
 
-    @Bean
-    public JmsListenerContainerFactory<?> containerFactory() {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setErrorHandler(new DefaultErrorHandler());
-        factory.setMessageConverter(messageConverter());
-        //Importante
-        factory.setSessionAcknowledgeMode(Session.AUTO_ACKNOWLEDGE);
-        factory.setSessionTransacted(true);
-        return factory;
-    }
+//     @Bean
+//     public JmsListenerContainerFactory<?> containerFactory() {
+//         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+//         factory.setConnectionFactory(connectionFactory);
+//         factory.setErrorHandler(new DefaultErrorHandler());
+//         factory.setMessageConverter(messageConverter());
+//         //Importante
+//         factory.setSessionAcknowledgeMode(Session.AUTO_ACKNOWLEDGE);
+//         factory.setSessionTransacted(true);
+//         return factory;
+//     }
 
-    @Bean
-    public MessageConverter messageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        return converter;
-    }
+//     @Bean
+//     public MessageConverter messageConverter() {
+//         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+//         converter.setTargetType(MessageType.TEXT);
+//         converter.setTypeIdPropertyName("_type");
+//         return converter;
+//     }
 
-}
+// }
